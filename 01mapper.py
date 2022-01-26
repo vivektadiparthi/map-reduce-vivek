1ccb6060fa9905d343a534dadbd5cdd82ff897bb
Vivek Tadiparthi <83790881+vivektadiparthi@users.noreply.github.com>
2022-01-26 15:07:11 -0600

    successful run

---
+++ 01mapper.py
@@ -0,0 +1,15 @@
+# Vivek
+# This is an example mapper
+
+f = open("purchases.txt","r")  # open file, read-only
+o = open("out01.txt", "w") # open file, write
+for line in f:  
+    dataList = line.strip().split("    ") 
+    print (dataList )
+    print (len(dataList ))
+    if len(dataList) == 6:
+        date, time, location, dept, amount, payType = dataList  #assign names
+        print ("{0}\t{1}".format(location, amount))
+        o.write("{0}\t{1}\n".format(location, amount))
+f.close()
+o.close()
